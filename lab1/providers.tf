abe808a3d76c1df1c2b39e21b24b1351567527b1
Simon Painter <simon.painter@gmail.com>
2025-01-29 20:11:03 +0000

    Lab templates 1 and 2 for subnet peering

---
+++ lab1/providers.tf
@@ -0,0 +1,19 @@
+provider "azurerm" {
+
+  client_id       = var.client_id
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+  client_secret   = var.client_secret
+
+
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+    virtual_machine {
+      delete_os_disk_on_deletion     = true
+      graceful_shutdown              = false
+      skip_shutdown_and_force_delete = true
+    }
+  }
+}
---
+++ lab1/variables.tf
@@ -0,0 +1,15 @@
+variable "client_secret" {
+  description = "Client Secret"
+}
+
+variable "client_id" {
+  description = "Client ID"
+}
+
+variable "tenant_id" {
+  description = "Tenant ID"
+}
+
+variable "subscription_id" {
+  description = "Subscription ID"
+}
