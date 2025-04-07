d8e8d654b3acc52a9ed0dead9431cc76a6fc06db
Simon Painter <simon.painter@gmail.com>
2025-04-07 09:34:48 +0100

    removed deprecated command

--- lab1/providers.tf
+++ lab1/providers.tf
@@ -5,15 +5,10 @@ provider "azurerm" {
   subscription_id = var.subscription_id
   client_secret   = var.client_secret
 
-
   features {
     resource_group {
       prevent_deletion_if_contains_resources = false
     }
-    virtual_machine {
-      delete_os_disk_on_deletion     = true
-      graceful_shutdown              = false
-      skip_shutdown_and_force_delete = true
-    }
+
   }
 }
